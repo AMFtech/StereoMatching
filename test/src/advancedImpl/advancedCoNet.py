6e0273202089ad14a698918e4e2ddb7303b63ecc
Georgi Dikov <gvdikov@gmail.com>
2015-10-31 20:38:56 +0100

    bugfixing and co.

--- test/src/advancedImpl/advancedCoNet.py
+++ test/src/advancedImpl/advancedCoNet.py
@@ -1,6 +1,6 @@
 from pyNN.nest import setup, run, end
 from network_builder import createCooperativeNetwork, createSpikeSource
-from plotter import plotSimulationResults
+# from plotter import plotSimulationResults
 
 
 ####### Network Parameters #########
@@ -20,20 +20,20 @@ setup(timestep=0.1, min_delay=0.2, max_delay=5.0)
 
 
 # create a spike sources firing at specific spiking times
-spikingTimingLeft = [[30.]]
+spikingTimingLeft = [[[30.], [300.], [300.]]]
 retinaLeft = createSpikeSource(dx = dimensionRetinaX, dy = dimensionRetinaY, timing = spikingTimingLeft)
-spikingTimingRight = [[40.]]
+spikingTimingRight = [[[40.], [300.], [300.]]]
 retinaRight = createSpikeSource(dx = dimensionRetinaX, dy = dimensionRetinaY, timing = spikingTimingRight)
 
 # create network and attach the spike sources 
-network = createCooperativeNetwork(dx = dimensionRetinaX, dy = dimensionRetinaY, dz = disparityMax, 
-	spikeSourceL = retinaLeft, spikeSourceR = retinaRight)
+# network = createCooperativeNetwork(dx = dimensionRetinaX, dy = dimensionRetinaY, dz = disparityMax, 
+# 	spikeSourceL = retinaLeft, spikeSourceR = retinaRight)
 
 # run simulation for time in milliseconds
-run(200.0)
+# run(200.0)
 
 # plot results
-plotSimulationResults(network, retinaLeft, retinaRight)
+# plotSimulationResults(network, retinaLeft, retinaRight)
 
 # finalise program and simulation
 end()
--- test/src/advancedImpl/network_builder.py
+++ test/src/advancedImpl/network_builder.py
@@ -6,28 +6,28 @@ This solution allows spiking to occur at arbitrary point within the simulation t
 '''
 def createSpikeSource(dx=1, dy=1, timing=[[]]):
 	if len(timing) < dy :
-		print "WARNING: Dimension of Timing Vector is mismatching the Spike Source's Dimension!" +
-			"Timing Vector will be automatically padded with []"
+		print "WARNING: Dimension of Timing Vector is mismatching the Spike Source's Dimension!"
+		"Timing Vector will be automatically padded with []"
 		for padding in range(0, dy - len(timing)):
 			timing.append([])
 	if dy >= 1:
 		oneLayer = None
 		if dx >= 1:
-			oneLayer = Population(1, SpikeSourceArray(spike_times = [timing[0][0]]), 
+			oneLayer = Population(1, SpikeSourceArray(spike_times = timing[0][0]), 
 				label="Left Retina -- Layer 1 -- Pixel 1")
 			for pixel in range(1, dx):
-				oneLayer += Population(1, SpikeSourceArray(spike_times = [timing[0][pixel]]), 
-					label="Left Retina -- Layer 1 -- Pixel %d", pixel+1)
+				oneLayer += Population(1, SpikeSourceArray(spike_times = timing[0][pixel]), 
+					label="Left Retina -- Layer 1 -- Pixel {0}".format(pixel+1))
 		else :
 			print "WARNING: No Spike Sources created! "
 			return oneLayer
 		spikeSource = oneLayer
 		for layer in range(1, dy):
-			oneLayer = Population(1, SpikeSourceArray(spike_times = [timing[layer][0]]), 
+			oneLayer = Population(1, SpikeSourceArray(spike_times = timing[layer][0]), 
 				label="Left Retina -- Layer 2 -- Pixel 1")
 			for pixel in range(1, dx):
-				oneLayer += Population(1, SpikeSourceArray(spike_times = [timing[layer][pixel]]), 
-					label="Left Retina -- Layer %d -- Pixel %d", layer+1, pixel+1)	
+				oneLayer += Population(1, SpikeSourceArray(spike_times = timing[layer][pixel]), 
+					label="Left Retina -- Layer {0} -- Pixel {1}".format(layer+1, pixel+1))	
 			spikeSource += oneLayer
 		return spikeSource
 	else :
@@ -35,5 +35,5 @@ def createSpikeSource(dx=1, dy=1, timing=[[]]):
 		
 
 
-def createCooperativeNetwork(dx=0, dy=0, dz=0, spikeSourceL, spikeSourceR):
+def createCooperativeNetwork(spikeSourceL, spikeSourceR, dx=0, dy=0, dz=0):
 	return None
