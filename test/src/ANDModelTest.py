f8397fc15d015a20db3d9eb35cac78a641e3243e
Georgi Dikov <gvdikov@gmail.com>
2015-11-03 13:04:00 +0100

    finetunning of parameters, temporary backup, not perfectly working

--- test/src/ANDModelTest.py
+++ test/src/ANDModelTest.py
@@ -16,24 +16,28 @@ neuronCell = Population(1, IF_curr_exp(), label="Cell Output")
 all_neurons = neuronInhLeft + neuronInhRight + neuronCell
 all_neurons.record('v')
 neuronCell.record('spikes')
+neuronInhLeft.record('spikes')
+neuronInhRight.record('spikes')
 
 all_neurons.set(tau_syn_E=1.0)
-all_neurons.set(tau_syn_I=1.0)
+all_neurons.set(tau_syn_I=1.06)
+all_neurons.set(tau_m=1.07)
+all_neurons.set(v_reset=-72.0)
 # create a spike source firing at spike_times
-retinaLeft = Population(1, SpikeSourceArray(spike_times=[50., 51., 52.]), label="Spike Source Left")
-retinaRight = Population(1, SpikeSourceArray(spike_times=[60.]), label="Spike Source Right")
+retinaLeft = Population(1, SpikeSourceArray(spike_times=[10., 11, 12, 13, 14, 15, 50., 70., 130.]), label="Spike Source Left")
+retinaRight = Population(1, SpikeSourceArray(spike_times=[16.0]), label="Spike Source Right")
 # connect them in according to the follwing pattern
 
-Projection(retinaLeft, neuronInhLeft, OneToOneConnector(), StaticSynapse(weight=22.5, delay=0.1))
-Projection(retinaLeft, neuronInhRight, OneToOneConnector(), StaticSynapse(weight=-22.5, delay=0.1))
-Projection(retinaLeft, neuronCell, OneToOneConnector(), StaticSynapse(weight=22.5, delay=1.0))
+Projection(retinaLeft, neuronInhLeft, OneToOneConnector(), StaticSynapse(weight=39.5, delay=0.1))
+Projection(retinaLeft, neuronInhRight, OneToOneConnector(), StaticSynapse(weight=-39.5, delay=0.1))
+Projection(retinaLeft, neuronCell, OneToOneConnector(), StaticSynapse(weight=39.5, delay=1.374))
 
-Projection(retinaRight, neuronInhRight, OneToOneConnector(), StaticSynapse(weight=22.5, delay=0.1))
-Projection(retinaRight, neuronInhLeft, OneToOneConnector(), StaticSynapse(weight=-22.5, delay=0.1))
-Projection(retinaRight, neuronCell, OneToOneConnector(), StaticSynapse(weight=22.5, delay=0.9))
+Projection(retinaRight, neuronInhRight, OneToOneConnector(), StaticSynapse(weight=39.5, delay=0.1))
+Projection(retinaRight, neuronInhLeft, OneToOneConnector(), StaticSynapse(weight=-39.5, delay=0.1))
+Projection(retinaRight, neuronCell, OneToOneConnector(), StaticSynapse(weight=39.5, delay=1.374))
 
-Projection(neuronInhLeft, neuronCell, OneToOneConnector(), StaticSynapse(weight=-22.5, delay=0.1))
-Projection(neuronInhRight, neuronCell, OneToOneConnector(), StaticSynapse(weight=-22.5, delay=0.1))
+Projection(neuronInhLeft, neuronCell, OneToOneConnector(), StaticSynapse(weight=-39.5, delay=0.1))
+Projection(neuronInhRight, neuronCell, OneToOneConnector(), StaticSynapse(weight=-39.5, delay=0.1))
 
 run(200.0)
 
@@ -49,14 +53,16 @@ Figure(
           data_labels=[neuronCell.label], yticks=True, xticks=True, ylim=(-75, -40)),
     Panel(neuronCell.get_data().segments[0].spiketrains, xlabel="Time (ms)", xticks=True),   
     Panel(neuronInhLeft.get_data().segments[0].filter(name='v')[0],
-          data_labels=[neuronInhLeft.label], yticks=True, xticks=True, ylim=(-75, -40)),
+          data_labels=[neuronInhLeft.label], yticks=True, xticks=True),
     Panel(neuronInhRight.get_data().segments[0].filter(name='v')[0],
-          data_labels=[neuronInhRight.label], yticks=True, xticks=True, ylim=(-75, -40)),
-    title="1Exc1Inh",
+          data_labels=[neuronInhRight.label], yticks=True, xticks=True),
+    title="AND-Cell",
     annotations="Simulated with NEST"
 ).save(figure_filename)
 print(figure_filename)
-print "Totally {0} spikes".format(len(neuronCell.get_data().segments[0].spiketrains[0]))
+print "Totally {0} spikes Cell Output".format(len(neuronCell.get_data().segments[0].spiketrains[0]))
+print "Totally {0} spikes Left Inhibitor".format(len(neuronInhLeft.get_data().segments[0].spiketrains[0]))
+print "Totally {0} spikes Right Inhibitor".format(len(neuronInhRight.get_data().segments[0].spiketrains[0]))
 
 end()
 
