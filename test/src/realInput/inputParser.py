31263d516448da29fa9de38e34651fdf3677d534
Georgi Dikov <gvdikov@gmail.com>
2015-11-27 00:43:36 +0100

    real input data parsing

---
+++ test/src/realInput/inputParser.py
@@ -0,0 +1,39 @@
+import cPickle	
+
+# fileName = "oneHand.dat"
+
+# eventList = []
+# with open(fileName, 'rb') as allEvents:
+# 	for line in allEvents:
+# 		eventList.append([int(x) for x in line.split()])
+		
+# # print eventList
+
+# dimRet = 128
+
+# retinaL = []
+# retinaR = []
+
+# for y in range(0, dimRet):
+# 	retinaR.append([])
+# 	retinaL.append([])
+# 	for x in range(0, dimRet):
+# 		retinaR[y].append([])
+# 		retinaL[y].append([])
+
+
+# for evt in eventList:
+# 	x = evt[1]-1
+# 	y = evt[2]-1
+
+# 	if evt[4] == 1:
+# 		retinaR[x][y].append(evt[0]/10000.0)
+# 	elif evt[4] == 0:
+# 		retinaL[x][y].append(evt[0]/10000.0)
+
+	
+# cPickle.dump(retinaL, open('retinaLeft.p', 'wb')) 	
+# cPickle.dump(retinaR, open('retinaRight.p', 'wb')) 	
+
+rL = cPickle.load(open('retinaLeft.p', 'rb'))
+print rL[94]
